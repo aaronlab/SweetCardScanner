8fe3cc6c54976547a1cf37704ac1691a27b72e77
Aaron Lee <aaronlab.net@gmail.com>
2020-11-14 15:04:46 +0900

    chore: Update package files
    Implement Reg Package

--- Package.swift
+++ Package.swift
@@ -5,22 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "SweetCardScanner",
+    
+    platforms: [.iOS(.v13)],
+    
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SweetCardScanner",
+            type: .dynamic,
             targets: ["SweetCardScanner"]),
     ],
+    
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/yhkaplan/Reg.git", from: "0.3.0"),
     ],
+    
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SweetCardScanner",
-            dependencies: []),
+            dependencies: ["Reg"]),
         .testTarget(
             name: "SweetCardScannerTests",
             dependencies: ["SweetCardScanner"]),
