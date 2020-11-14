c6fe3fda1247f4a8af50e382ed1d028565a6a4e3
Aaron Lee <aaronlab.net@gmail.com>
2020-11-14 15:03:49 +0900

    feat: Implement CreditCardScanner
    https://github.com/yhkaplan/credit-card-scanner

---
+++ CreditCardScanner/CameraView.swift
@@ -0,0 +1,210 @@
+//
+//  CameraView.swift
+//  CreditCardScannerPackageDescription
+//
+//  Created by josh on 2020/07/23.
+//
+
+import AVFoundation
+import UIKit
+import VideoToolbox
+
+protocol CameraViewDelegate: AnyObject {
+    func didCapture(image: CGImage)
+    func didError(with: CreditCardScannerError)
+}
+
+final class CameraView: UIView {
+    weak var delegate: CameraViewDelegate?
+
+    // MARK: - Capture related
+
+    private let captureSessionQueue = DispatchQueue(
+        label: "com.yhkaplan.credit-card-scanner.captureSessionQueue"
+    )
+
+    // MARK: - Capture related
+
+    private let sampleBufferQueue = DispatchQueue(
+        label: "com.yhkaplan.credit-card-scanner.sampleBufferQueue"
+    )
+
+    init(delegate: CameraViewDelegate) {
+        self.delegate = delegate
+        super.init(frame: .zero)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private let imageRatio: ImageRatio = .vga640x480
+
+    // MARK: - Region of interest and text orientation
+
+    /// Region of video data output buffer that recognition should be run on.
+    /// Gets recalculated once the bounds of the preview layer are known.
+    private var regionOfInterest: CGRect?
+
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+            fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
+        }
+
+        return layer
+    }
+
+    private var videoSession: AVCaptureSession? {
+        get {
+            videoPreviewLayer.session
+        }
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+
+    let semaphore = DispatchSemaphore(value: 1)
+
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
+    }
+
+    func stopSession() {
+        videoSession?.stopRunning()
+    }
+
+    func startSession() {
+        videoSession?.startRunning()
+    }
+
+    func setupCamera() {
+        captureSessionQueue.async { [weak self] in
+            self?._setupCamera()
+        }
+    }
+
+    private func _setupCamera() {
+        let session = AVCaptureSession()
+        session.beginConfiguration()
+        session.sessionPreset = imageRatio.preset
+
+        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                        for: .video,
+                                                        position: .back) else {
+            delegate?.didError(with: CreditCardScannerError(kind: .cameraSetup))
+            return
+        }
+
+        do {
+            let deviceInput = try AVCaptureDeviceInput(device: videoDevice)
+            session.canAddInput(deviceInput)
+            session.addInput(deviceInput)
+        } catch {
+            delegate?.didError(with: CreditCardScannerError(kind: .cameraSetup, underlyingError: error))
+        }
+
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.alwaysDiscardsLateVideoFrames = true
+        videoOutput.setSampleBufferDelegate(self, queue: sampleBufferQueue)
+
+        guard session.canAddOutput(videoOutput) else {
+            delegate?.didError(with: CreditCardScannerError(kind: .cameraSetup))
+            return
+        }
+
+        session.addOutput(videoOutput)
+        session.connections.forEach {
+            $0.videoOrientation = .portrait
+        }
+        session.commitConfiguration()
+
+        DispatchQueue.main.async { [weak self] in
+            self?.videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            self?.videoSession = session
+            self?.startSession()
+        }
+    }
+
+    func setupRegionOfInterest() {
+        guard regionOfInterest == nil else { return }
+        /// Mask layer that covering area around camera view
+        let backLayer = CALayer()
+        backLayer.frame = bounds
+
+        //  culcurate cutoutted frame
+        let cuttedWidth: CGFloat = bounds.width - 40.0
+        let cuttedHeight: CGFloat = cuttedWidth * CreditCard.heightRatioAgainstWidth
+
+        let centerVertical = (bounds.height / 2.0)
+        let cuttedY: CGFloat = centerVertical - (cuttedHeight / 2.0)
+        let cuttedX: CGFloat = 20.0
+
+        let cuttedRect = CGRect(x: cuttedX,
+                                y: cuttedY,
+                                width: cuttedWidth,
+                                height: cuttedHeight)
+
+        let maskLayer = CAShapeLayer()
+        let path = UIBezierPath(roundedRect: cuttedRect, cornerRadius: 10.0)
+
+        path.append(UIBezierPath(rect: bounds))
+        maskLayer.path = path.cgPath
+        maskLayer.fillRule = .evenOdd
+        backLayer.mask = maskLayer
+        layer.addSublayer(backLayer)
+
+        let strokeLayer = CAShapeLayer()
+        strokeLayer.lineWidth = 3.0
+        strokeLayer.path = UIBezierPath(roundedRect: cuttedRect, cornerRadius: 10.0).cgPath
+        strokeLayer.fillColor = nil
+        layer.addSublayer(strokeLayer)
+
+        let imageHeight: CGFloat = imageRatio.imageHeight
+        let imageWidth: CGFloat = imageRatio.imageWidth
+
+        let acutualImageRatioAgainstVisibleSize = imageWidth / bounds.width
+        let interestX = cuttedRect.origin.x * acutualImageRatioAgainstVisibleSize
+        let interestWidth = cuttedRect.width * acutualImageRatioAgainstVisibleSize
+        let interestHeight = interestWidth * CreditCard.heightRatioAgainstWidth
+        let interestY = (imageHeight / 2.0) - (interestHeight / 2.0)
+        regionOfInterest = CGRect(x: interestX,
+                                  y: interestY,
+                                  width: interestWidth,
+                                  height: interestHeight)
+    }
+}
+
+extension CameraView: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        semaphore.wait()
+        defer { semaphore.signal() }
+
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            delegate?.didError(with: CreditCardScannerError(kind: .capture))
+            delegate = nil
+            return
+        }
+
+        var cgImage: CGImage?
+        VTCreateCGImageFromCVPixelBuffer(pixelBuffer, options: nil, imageOut: &cgImage)
+
+        guard let regionOfInterest = regionOfInterest else {
+            return
+        }
+
+        guard let fullCameraImage = cgImage,
+            let croppedImage = fullCameraImage.cropping(to: regionOfInterest) else {
+            delegate?.didError(with: CreditCardScannerError(kind: .capture))
+            delegate = nil
+            return
+        }
+
+        delegate?.didCapture(image: croppedImage)
+    }
+}
+
+extension CreditCard {
+    // The aspect ratio of credit-card is Golden-ratio
+    static let heightRatioAgainstWidth: CGFloat = 0.6180469716
+}
---
+++ CreditCardScanner/CreditCardScannerViewController.swift
@@ -0,0 +1,136 @@
+//  Created by josh on 2020/07/23.
+
+import AVFoundation
+import UIKit
+
+public protocol CreditCardScannerViewControllerDelegate: AnyObject {
+    /// Called user taps the cancel button. Comes with a default implementation for UIViewControllers.
+    /// - Warning: The viewController does not auto-dismiss. You must dismiss the viewController
+    func creditCardScannerViewControllerDidCancel(_ viewController: CreditCardScannerViewController)
+    /// Called when an error is encountered
+    func creditCardScannerViewController(_ viewController: CreditCardScannerViewController, didErrorWith error: CreditCardScannerError)
+    /// Called when finished successfully
+    /// - Note: successful finish does not guarentee that all credit card info can be extracted
+    func creditCardScannerViewController(_ viewController: CreditCardScannerViewController, didFinishWith card: CreditCard)
+}
+
+public extension CreditCardScannerViewControllerDelegate where Self: UIViewController {
+    func creditCardScannerViewControllerDidCancel(_ viewController: CreditCardScannerViewController) {
+        viewController.dismiss(animated: true)
+    }
+}
+
+open class CreditCardScannerViewController: UIViewController {
+    /// public propaties
+    
+    // MARK: - Subviews and layers
+    
+    /// View representing live camera
+    private lazy var cameraView: CameraView = CameraView(delegate: self)
+    
+    /// Analyzes text data for credit card info
+    private lazy var analyzer = ImageAnalyzer(delegate: self)
+    
+    private weak var delegate: CreditCardScannerViewControllerDelegate?
+    
+    // MARK: - Vision-related
+    
+    public init(delegate: CreditCardScannerViewControllerDelegate?) {
+        self.delegate = delegate
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError("Not implemented")
+    }
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        layoutSubviews()
+        AVCaptureDevice.authorize { [weak self] authoriazed in
+            // This is on the main thread.
+            guard let strongSelf = self else {
+                return
+            }
+            guard authoriazed else {
+                strongSelf.delegate?.creditCardScannerViewController(strongSelf, didErrorWith: CreditCardScannerError(kind: .authorizationDenied, underlyingError: nil))
+                return
+            }
+            strongSelf.cameraView.setupCamera()
+        }
+    }
+    
+    override open func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        cameraView.setupRegionOfInterest()
+    }
+}
+
+private extension CreditCardScannerViewController {
+    @objc func cancel(_ sender: UIButton) {
+        delegate?.creditCardScannerViewControllerDidCancel(self)
+    }
+    
+    func layoutSubviews() {
+        cameraView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(cameraView)
+        cameraView.frame = view.frame
+    }
+    
+}
+
+extension CreditCardScannerViewController: CameraViewDelegate {
+    internal func didCapture(image: CGImage) {
+        analyzer.analyze(image: image)
+    }
+    
+    internal func didError(with error: CreditCardScannerError) {
+        DispatchQueue.main.async { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.delegate?.creditCardScannerViewController(strongSelf, didErrorWith: error)
+            strongSelf.cameraView.stopSession()
+        }
+    }
+}
+
+extension CreditCardScannerViewController: ImageAnalyzerProtocol {
+    internal func didFinishAnalyzation(with result: Result<CreditCard, CreditCardScannerError>) {
+        switch result {
+        case let .success(creditCard):
+            DispatchQueue.main.async { [weak self] in
+                guard let strongSelf = self else { return }
+                strongSelf.cameraView.stopSession()
+                strongSelf.delegate?.creditCardScannerViewController(strongSelf, didFinishWith: creditCard)
+            }
+            
+        case let .failure(error):
+            DispatchQueue.main.async { [weak self] in
+                guard let strongSelf = self else { return }
+                strongSelf.cameraView.stopSession()
+                strongSelf.delegate?.creditCardScannerViewController(strongSelf, didErrorWith: error)
+            }
+        }
+    }
+}
+
+extension AVCaptureDevice {
+    static func authorize(authorizedHandler: @escaping ((Bool) -> Void)) {
+        let mainThreadHandler: ((Bool) -> Void) = { isAuthorized in
+            DispatchQueue.main.async {
+                authorizedHandler(isAuthorized)
+            }
+        }
+        
+        switch authorizationStatus(for: .video) {
+        case .authorized:
+            mainThreadHandler(true)
+        case .notDetermined:
+            requestAccess(for: .video, completionHandler: { granted in
+                mainThreadHandler(granted)
+            })
+        default:
+            mainThreadHandler(false)
+        }
+    }
+}
