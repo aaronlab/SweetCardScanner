24e80ee565c59dde58e87959f654041a2086fd1a
Aaron Lee <aaronlab.net@gmail.com>
2020-11-30 23:39:14 +0900

    chore: Edit CreditCard model

--- Sources/SweetCardScanner/CreditCardScanner/CreditCard.swift
+++ Sources/SweetCardScanner/CreditCardScanner/CreditCard.swift
@@ -11,4 +11,8 @@ public struct CreditCard {
     public var number: String?
     public var name: String?
     public var expireDate: DateComponents?
+    public var year: Int { expireDate?.year ?? 0 }
+    public var month: Int { expireDate?.month ?? 0 }
+    public var vendor: CardVendor { CreditCardUtil.getVendor(candidate: self.number) }
+    public var isNotExpired: Bool? { CreditCardUtil.isValid(candidate: self.expireDate) }
 }
