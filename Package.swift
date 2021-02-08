4a63b9310780748946ff25c32326ff60b7830f5c
Aaron Lee <aaronlab.net@gmail.com>
2021-02-08 23:14:02 +0900

    chore: remove dynamic type

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
     products: [
         .library(
             name: "SweetCardScanner",
-            type: .dynamic,
             targets: ["SweetCardScanner"]),
     ],
     
